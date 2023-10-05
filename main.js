a8707d0c96fc53f6e8cb2690a18ad3ffda1199fc
Abhishek Tyagi <abhishektyagigi4310@gmail.com>
2023-10-05 11:30:25 +0530

    Added calculator functions and calculator UI

---
+++ main.js
@@ -0,0 +1,27 @@
+let firstNumber;
+let secondNumber;
+let theOperator;
+
+
+const add = function(a,b){
+    return a+b;
+}
+
+const subtract = function(a,b){
+
+    return a-b;
+}
+
+const multiply = function(a,b){
+
+    return a*b;
+}
+
+const divide = function(a,b){
+
+    return a/b;
+}
+
+const operate = function(operator,num1,num2,callback){
+
+}
