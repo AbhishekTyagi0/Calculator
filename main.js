a7e5ad6ef4bbd99d970bda3dac9c98c607c80180
Abhishek Tyagi <abhishektyagigi4310@gmail.com>
2023-10-06 11:20:51 +0530

    create a basic calculator after styling made a basic calculator using DOM.

--- main.js
+++ main.js
@@ -1,27 +1,93 @@
-let firstNumber;
-let secondNumber;
-let theOperator;
 
 
-const add = function(a,b){
-    return a+b;
+const add = function (a, b) {
+    return a + b;
 }
 
-const subtract = function(a,b){
+const substract = function (a, b) {
 
-    return a-b;
+    return a - b;
 }
 
-const multiply = function(a,b){
+const multiply = function (a, b) {
 
-    return a*b;
+    return a * b;
 }
 
-const divide = function(a,b){
+const divide = function (a, b) {
 
-    return a/b;
+    return a / b;
 }
 
-const operate = function(operator,num1,num2,callback){
+const operate = function (operator, num1, num2) {
 
+    a = Number(num1);
+    b = Number(num2);
+
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return substract(a, b)
+        case '*':
+            return multiply(a, b)
+        case '/':
+            if (b === 0) return null
+            else return divide(a, b)
+        default:
+            return null
+    }
+}
+
+
+let firstNumber = '';
+let previousNumber = '';
+let theOperator = null;
+
+
+const current = document.querySelector('.current');
+const operators = document.querySelectorAll('.operator');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+const buttons = document.querySelectorAll('.btn');
+const previous = document.querySelector('.previous');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        btnNumber(e.target.textContent);
+    });
+});
+
+function btnNumber(num) {
+    firstNumber += num;
+    current.textContent = firstNumber;
+}
+
+operators.forEach(button => {
+    button.addEventListener('click', function (e) {
+        op(e.target.textContent);
+    });
+});
+
+function op(op) {
+    previousNumber = `${firstNumber} ${op} `;
+    theOperator = op;
+    previous.textContent = previousNumber;
+    current.textContent = ' ';
+}
+
+
+equal.addEventListener('click', function () {
+
+    output();
+});
+
+function output() {
+
+    if (theOperator && previousNumber) {
+        firstNumber = operate(theOperator, current.textContent, firstNumber);
+        current.textContent = firstNumber;
+        previousNumber = '';
+        theOperator = null;
+    }
 }
